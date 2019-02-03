eb825b002f96ea013a1b9a3b2dd44e8e392c119d
Carlos Zoido <mrgalleta@gmail.com>
2019-02-03 19:15:36 +0100

    moved build_policy to build.py seemed not to work in conanfile.py

--- build.py
+++ build.py
@@ -4,6 +4,6 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     os.environ["CONAN_REMOTES"]="https://api.bintray.com/conan/czoido/conan-packages@True@czoido-bintray"
-    builder = ConanMultiPackager()
+    builder = ConanMultiPackager(build_policy="missing")
     builder.add_common_builds()
     builder.run()
