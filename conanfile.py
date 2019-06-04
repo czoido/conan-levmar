145266e0e8d58813f03d1c7f3af4bb76cb2d9ce0
czoido <mrgalleta@gmail.com>
2019-06-04 19:03:36 +0200

    minor changes

--- conanfile.py
+++ conanfile.py
@@ -49,7 +49,6 @@ conan_basic_setup()''')
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
-        self.copy("*.lib", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ["levmar"]
